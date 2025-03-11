5069492364bfb72973daf5904544c9c1bbf9fd5f
Jeremie-Braillon <jeremie.braillon@gmail.com>
2025-03-11 16:27:31 +0100

    Etape 1 : afficher le type de bouton dans la console

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-// NOTE: 
-// This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
+//Jérémie Braillon
 
-// # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+
+
+//Etape 1 : Cliquer sur les boutons chiffres et les afficher dans la console
+//Etape 2 : Afficher les chiffres dans l'écran de la calculatrice
+//Etape 3 : Afficher les nombres dans l'écran de la calculatrice
+//Etape 4 : Afficher les opérations dans l'écran de la calculatrice
+//Etape 5 : Réaliser les calculs en appuyant sur le bouton égal
+//Etape 6 : Réaliser les calculs avec les nombres à virgule
+
+
+//Etape 1 :
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+    const key = e.target
+    const action = key.dataset.action
+    if (key.matches('button')) {
+        //console.log('button clicked', e.target.innerText)
+        if (!action){
+            console.log('number key!')
+        }
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide'){
+            console.log('operator key!')
+        }
+        if (action === 'decimal'){
+            console.log('decimal key!')
+        }   
+        if (action === 'clear'){
+            console.log('clear key!')
+        }
+        if (action === 'calculate'){
+            console.log('equal key!')
+        }
+  }
+})
+
+
+//Etape 2 :
+
+const display = document.querySelector('.calculator__display')
+
