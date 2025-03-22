abd64c279dbf90c18f6e06ae7f03bc6241fb96d7
Jeremie-Braillon <jeremie.braillon@gmail.com>
2025-03-22 12:25:25 +0100

    Devoir complété

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 
 
-//Etape 1 : Cliquer sur les boutons chiffres et les afficher dans la console
-//Etape 2 : Afficher les chiffres dans l'écran de la calculatrice
-//Etape 3 : Afficher les nombres dans l'écran de la calculatrice
-//Etape 4 : Afficher les opérations dans l'écran de la calculatrice
-//Etape 5 : Réaliser les calculs en appuyant sur le bouton égal
+//Etape 1 : Cliquer sur les boutons chiffres et les afficher dans la console  --> Done
+//Etape 2 : Afficher les chiffres dans l'écran de la calculatrice --> Done
+//Etape 3 : Afficher les nombres dans l'écran de la calculatrice --> Done
+//Etape 4 : Afficher les opérations dans l'écran de la calculatrice --> Done 
+//Etape 5 : Réaliser les calculs en appuyant sur le bouton égal --> Done
 //Etape 6 : Réaliser les calculs avec les nombres à virgule
 
 
@@ -17,7 +17,9 @@ const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
 let calcul = null;
 let resultat = 0;
-let lastButtonClass=null;
+let operator=false;
+let number=false;
+let divByZero = false;
 
 keys.addEventListener('click', e => {
     const key = e.target
@@ -27,27 +29,66 @@ keys.addEventListener('click', e => {
     const buttonclass = key.class
     
     if (key.matches('button')) {
-        if (!action){  //Clique sur une case numéro
-            if (lastButtonClasslass === "key--operator"){
-                display.textContent = '0'
-            }
-
-            if (displayedNum === '0') {
+        if (!action && !divByZero){  //Clique sur une case numéro
+            if (operator){
+                display.textContent = keyContent
+                operator = false
+            } else if (displayedNum === '0') {
                 display.textContent = keyContent
             } else {
                 display.textContent = display.textContent + keyContent
             }
+            number=true
 
-        } else if(buttonclass === "key--operator"){
-
-            resultat = parseInt(displayedNum)
-            display.textContent += keyContent
+        } else if (action === "decimal" && !divByZero){
+            display.textContent += '.'
+        } else if (action === "clear"){
+            display.textContent = '0'
+            divByZero = false
+            number = false
+            operator = false
+            calcul = null
+        } else if ((action === "multiply" || action === "add" || action === "subtract" || action === "divide") && !divByZero && number){
+            if (calcul==="multiply"){
+                resultat = resultat*parseFloat(displayedNum)
+            } else if (calcul === "add"){
+                resultat += parseFloat(displayedNum)
+            } else if (calcul === "subtract"){
+                resultat -= parseFloat(displayedNum)
+            } else if (calcul === "divide"){
+                if (displayedNum != '0'){
+                    resultat = resultat/parseFloat(displayedNum)
+                } else{
+                    display.textContent = "Div by 0, press AC"
+                    divByZero = true
+                } 
+            }else {
+                resultat = parseFloat(displayedNum)
+            }
+            number = false
             calcul = action
-
+            operator = true
+        } else if (action === "calculate" && number){
+            if (calcul==="multiply"){
+                resultat = resultat*parseFloat(displayedNum)
+            } else if (calcul === "add"){
+                resultat += parseFloat(displayedNum)
+            } else if (calcul === "subtract"){
+                resultat -= parseFloat(displayedNum)
+            } else if (calcul === "divide"){
+                if (displayedNum != '0'){
+                    resultat = resultat/parseFloat(displayedNum)
+                } else{
+                    display.textContent = "Div by 0, press AC"
+                    divByZero = true
+                } 
+            }else {
+                resultat = parseFloat(displayedNum)
+            }
+            if (!divByZero){
+                display.textContent = resultat
+                calcul = null
+            }
         }
-        lastButtonClass=buttonclass
-
-
-
     }
 })
